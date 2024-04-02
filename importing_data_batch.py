fcc2e4555c081ef88edb4951a0aad1db5cb081aa
CFVPXV <84998466+CFVPXV@users.noreply.github.com>
2024-04-02 20:16:05 +0000

    making headway

---
+++ importing_data_batch.py
@@ -0,0 +1,11 @@
+import os
+
+with open("ingestion.txt", "r") as file:
+    all_data = file.readlines()
+
+for cmd in all_data:
+    os.system("")
+
+
+
+os.listdir()
--- mainly.py
+++ mainly.py
@@ -4,17 +4,44 @@ import math
 import numpy as np
 import itertools
 import cv2
-import matplotlib.pyplot as plt
+import os
+import json
 
 from waymo_open_dataset.utils import range_image_utils
 from waymo_open_dataset.utils import transform_utils
 from waymo_open_dataset.utils import  frame_utils
 from waymo_open_dataset import dataset_pb2 as open_dataset
 
-FILENAME = "data/waymotfrecord1"
+#FILENAME = "data/waymotfrecord1"
 
-dataset = tf.data.TFRecordDataset(FILENAME, compression_type='')
+# Look for all possible categories later...
+final_json = {"images": [], "annotations": [], "categories": []}
 
+files = os.listdir("data/")
+
+#print(files)
+
+item = 0
+os.system("mkdir intermediate")
+
+for i in files:
+    dataset = tf.data.TFRecordDataset(f"data/{i}", compression_type='')
+    for data in dataset:
+      frame = open_dataset.Frame()
+      frame.ParseFromString(bytearray(data.numpy()))
+      break
+
+    for index, image in enumerate(frame.images):
+      item += 1
+      filename = f"img{item}.jpg"
+      final_json["images"].append({"id": item, "file_name": filename})
+      cv2.imwrite(f"intermediate/{filename}", np.array(tf.image.decode_jpeg(image.image)))
+      print(item)
+
+
+with open("intermediate/labels.json", "w") as outfile:
+   json.dump(final_json, outfile)
+"""
 for data in dataset:
   frame = open_dataset.Frame()
   frame.ParseFromString(bytearray(data.numpy()))
@@ -25,7 +52,7 @@ for index, image in enumerate(frame.images):
   item += 1
   cv2.imwrite(f"img{item}.jpg", np.array(tf.image.decode_jpeg(image.image)))
   print(item)
-
+"""
 
 
 #print(frame.camera_labels[0])
